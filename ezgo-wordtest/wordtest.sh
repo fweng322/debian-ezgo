c37efbff9c2059d8a6056ae8c980928b2a7316d7
Franklin <franklin@goodhorse.idv.tw>
2015-01-24 10:08:26 +0800

    Add a comment and use variable for path

--- ezgo-wordtest/wordtest.sh
+++ ezgo-wordtest/wordtest.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+# Franklin.20150123: It's better to run wordtest with firefox or iceweasel, because google would not allow user to access local files.
+
 FIREFOX=`which firefox`
 ICEWEASEL=`which iceweasel`
 CHROME=`which google-chrome`
 CHROMIUM=`which chromium-browser`
 
-WORDTEST=
+WORDTEST=/usr/share/ezgo/ezgo-wordtest/index.html
 
 if [ ! -z "${FIREFOX}" ]; then	# use firefox
-	${FIREFOX} /usr/share/ezgo/ezgo-wordtest/index.html
+	${FIREFOX} ${WORDTEST}
 elif [ ! -z "${ICEWEASEL}" ]; then # use iceweasel
-	${ICEWEASEL} /usr/share/ezgo/ezgo-wordtest/index.html
+	${ICEWEASEL} ${WORDTEST}
 elif [ ! -z "${CHROMIUM}" ]; then # use chromium
 	${CHROMIUM} /usr/share/ezgo/ezgo-wordtest/start_server.sh
 elif [ ! -z "${CHROME}" ]; then # use google-chrome
 	${CHROME} /usr/share/ezgo/ezgo-wordtest/start_server.sh
 else
-	xdg-open /usr/share/ezgo/ezgo-wordtest/index.html
+	xdg-open ${WORDTEST}
 fi
 
