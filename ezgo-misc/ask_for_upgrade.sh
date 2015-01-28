cb00d4feab471b5402b1a22064a888631472ee56
Franklin <franklin@goodhorse.idv.tw>
2015-01-29 00:16:58 +0800

    Add upgrade shell scripts for ezgo users

---
+++ ezgo-misc/ask_for_upgrade.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+kdialog --yesno "系統將進行更新。注意：此動作一旦開始就無法中止，否則可能造成系統問題。要繼續嗎？"
+
+ans=$?
+
+if [ $ans -eq 0 ]; then
+        gksudo -- sed -ri 's/tw.archive.ubuntu.com/free.nchc.org.tw/' /etc/apt/sources.list /etc/apt/sources.list.d/*
+        gksudo -- sed -ri 's/security.ubuntu.com/free.nchc.org.tw/' /etc/apt/sources.list /etc/apt/sources.list.d/*
+	gksudo -- apt-get update
+	gksudo -- apt-get -y upgrade
+#	gksudo -- /usr/share/ezgo/ezgomenu/desktop_maker.py
+fi
