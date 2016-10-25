3694e2b55692233e279d493d4bc0abdd5f4ea481
Franklin Weng <franklin@goodhorse.idv.tw>
2016-10-25 20:32:01 +0800

    kde5 setting and splash updated

--- ezgo-kde5/kde/defaultPanel.layout.js
+++ ezgo-kde5/kde/defaultPanel.layout.js
@@ -32,7 +32,7 @@ kickoff.currentConfigGroup = ["General"]
 kickoff.writeConfig("useCustomButtonImage", "true")
 kickoff.writeConfig("customButtonImage", "/usr/share/ezgo/icons/start_menu.svg")
 kickoff.writeConfig("favoriteApps", "libreoffice-writer.desktop,oxoffice6-writer.desktop,libreoffice-calc.desktop,oxoffice6-calc.desktop,libreoffice-impress.desktop,oxoffice6-impress.desktop,inkscape.desktop,gimp.desktop,org.kde.konsole.desktop,phet.desktop")
-kickoff.writeConfig("hiddenApplications", "display-im6.q16.desktop,display-im6.desktop,org.kde.ksshaskpass.desktop,JB-javaws-jdk8.desktop,org.kde.knetattach.desktop,JB-mission-control-jdk8.desktop,fcitx.desktop,nautilus.desktop,org.kde.kwalletmanager5.desktop,JB-jvisualvm-jdk8.desktop")
+kickoff.writeConfig("hiddenApplications", "display-im6.q16.desktop,display-im6.desktop,org.kde.ksshaskpass.desktop,JB-javaws-jdk8.desktop,org.kde.knetattach.desktop,JB-mission-control-jdk8.desktop,fcitx.desktop,nautilus.desktop,org.kde.kwalletmanager5.desktop,JB-jvisualvm-jdk8.desktop,JB-controlpanel-jdk8.desktop,JB-jconsole-jdk8.desktop,itweb-settings.desktop,openjdk-8-policytool.desktop,fcitx.desktop,org.kde.kwalletmanager5.desktop")
 kickoff.currentConfigGroup = ["Shortcuts"]
 kickoff.writeConfig("global", "Alt+F1")
 
