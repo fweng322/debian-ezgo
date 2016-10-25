0e83f6f6f8f55c1f48378c0d3accd26580893751
Franklin Weng <franklin@goodhorse.idv.tw>
2016-10-25 20:48:53 +0800

    fix ezgo-decompress in lxde

--- ezgo-misc/ezgo-decompress/decompress.sh
+++ ezgo-misc/ezgo-decompress/decompress.sh
@@ -5,4 +5,10 @@ if [ {$1}z = 'z' ]; then
 	exit
 fi
 
-7za x -o`kdialog --getexistingdirectory $PWD` $1
+if [ -x /usr/bin/kdialog ]; then
+  7za x -o`kdialog --title "請選擇解壓縮的檔案要放在哪裡" --getexistingdirectory $PWD` $1
+elif [ -x /usr/bin/zenity ]; then
+  7za x -o`zenity --title "請選擇解壓縮的檔案要放在哪裡" --file-selection --directory` $1
+else
+  7za x -o$HOME $1
+fi
