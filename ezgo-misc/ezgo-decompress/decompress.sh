30a756ba7ce0f43531a6b2bfc486545d60be5a4c
Franklin Weng <franklin@goodhorse.idv.tw>
2016-11-05 14:23:36 +0800

    Improve ezgo-decompress

--- ezgo-misc/ezgo-decompress/decompress.sh
+++ ezgo-misc/ezgo-decompress/decompress.sh
@@ -5,10 +5,38 @@ if [ {$1}z = 'z' ]; then
 	exit
 fi
 
+
 if [ -x /usr/bin/kdialog ]; then
-  7za x -o`kdialog --title "請選擇解壓縮的檔案要放在哪裡" --getexistingdirectory $PWD` $1
+  DIALOG=kdialog
+  DECOMPRESS_METHOD=`kdialog --title "請選擇解壓縮方式" --combobox "請選擇要使用的解壓縮方式。\n若選擇的方式解出來是亂碼，可以換其他種方式試試。" "unzip" "7z" "unrar" --default "unzip"`
+  FOLDER=`kdialog --title "請選擇解壓縮的檔案要放在哪裡" --getexistingdirectory "$PWD"`
 elif [ -x /usr/bin/zenity ]; then
-  7za x -o`zenity --title "請選擇解壓縮的檔案要放在哪裡" --file-selection --directory` $1
+  DIALOG=zenity
+  DECOMPRESS_METHOD=`zenity --list --radiolist --text "請選擇要解壓縮的方式" --column "button" --column "method" --hide-header TRUE unzip FALSE 7z FALSE unrar`
+  FOLDER=`zenity --title "請選擇解壓縮的檔案要放在哪裡" --file-selection --directory --filename="$PWD"`
 else
-  7za x -o$HOME $1
+  DECOMPRESS_METHOD=unzip
+  FOLDER="$HOME"
 fi
+
+case $DECOMPRESS_METHOD in
+  unzip)
+    echo "User chose unzip."
+    unzip "$1" -d "$FOLDER"
+    ;;
+  7z)
+    echo "User chose 7z."
+    7za x -o"$FOLDER" "$1"
+    ;;
+  unrar)
+    echo "User chose unrar."
+    unrar x "$1" "$FOLDER/"
+    ;;
+  *)
+    echo "Use default option unzip."
+    unzip "$1" -d "$FOLDER"
+    ;;
+esac
+
+exit
+
